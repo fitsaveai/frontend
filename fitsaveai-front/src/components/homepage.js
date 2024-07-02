8425754b01c88eba3557494a12e55bb7a053ef6c
Shawn <depedroshawn@gmail.com>
2024-07-02 18:17:31 -0400

    Push #1

--- fitsaveai-front/src/components/homepage.js
+++ fitsaveai-front/src/components/homepage.js
@@ -19,8 +19,8 @@ const HomePage = () => {
                 <h1>Build Workouts Faster</h1>
                 <p>FitSaverAI is an AI-powered workout builder for building workouts fast.</p>
                 <div className="cta-buttons">
-                    <button className="btn btn-secondary">Sign In</button>
-                    <button className="btn btn-primary">Register</button>
+                    <button className="btn btn-secondary1">Sign In</button>
+                    <button className="btn btn-primary1">Register</button>
                 </div>
             </main>
         </div>
