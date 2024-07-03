2b614154076e655d735a0733112a29fd6b554864
arnavt687 <arnav144193@gmail.com>
2024-07-03 02:32:52 -0400

    push 2

--- fitsaveai-front/src/components/homepage.js
+++ fitsaveai-front/src/components/homepage.js
@@ -16,13 +16,29 @@ const HomePage = () => {
                 </div>
             </nav>
             <main className="hero">
+                
                 <h1>Build Workouts Faster</h1>
                 <p>FitSaverAI is an AI-powered workout builder for building workouts fast.</p>
                 <div className="cta-buttons">
                     <button className="btn btn-secondary">Sign In</button>
                     <button className="btn btn-primary">Register</button>
                 </div>
+                <div className="boxes-container">
+                    <div className="box">
+                        <h2>Chest Day</h2>
+                        <p>Description of Chest workouts...</p>
+                    </div>
+                    <div className="box">
+                        <h2>Arms</h2>
+                        <p>Description of Arm workouts...</p>
+                    </div>
+                    <div className="box">
+                        <h2>Legs</h2>
+                        <p>Description of Legs workouts...</p>
+                    </div>
+                </div>
             </main>
+
             <footer className="footer">
             
             <div className="footer-contact">
