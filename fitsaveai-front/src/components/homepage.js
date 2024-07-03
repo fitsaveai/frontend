e0825a37cbabc4aade7c7368c02dcd84268b379d
kevin42 <kevinkokinda1@gmail.com>
2024-07-03 01:30:16 -0400

    save

--- fitsaveai-front/src/components/homepage.js
+++ fitsaveai-front/src/components/homepage.js
@@ -13,16 +13,6 @@ const HomePage = () => {
                     <Link to="/register" className="btn btn-primary">Register</Link>
                 </div>
             </main>
-            <footer className="footer">
-            
-            <div className="footer-contact">
-                    <p>Contact Us: something@fitsaverai.com</p>
-                    < p>Phone: +1 234 567 890</p>
-                </div>
-                <div className="footer-cta">
-                    <button className="btn btn-primary">Get Started</button>
-                </div>
-            </footer>
         </div>
     );
 };
