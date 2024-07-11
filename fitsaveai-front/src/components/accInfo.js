52929011e2007a0b084aeeb790666fdbc29750d4
Shawnycute <depedroshawn@gmail.com>
2024-07-10 21:26:39 -0400

    stuff

--- fitsaveai-front/src/components/accInfo.js
+++ fitsaveai-front/src/components/accInfo.js
@@ -10,35 +10,39 @@ const ExplorePage = () => {
     const tokenCopy = document.getElementById('token');
     const name = localStorage.getItem('name');
     const email = localStorage.getItem('email');
-    const token = localStorage.getItem('token');
     const [error, setError] = useState('');
     const navigate = useNavigate();
     const {changeUName}  = useContext(AuthContext);
-
+    const token = localStorage.getItem('token');
     const changeName = async (e) => {
+        // console.log(token);
         e.preventDefault();
         const name = document.getElementById("NameChange").value;
         setError('');
+        if (token) {
+            try {
+                // Log the value of changeUName for debugging
+                // console.log('changeUName:', changeUName.changeUName);
     
-        try {
-            // Log the value of changeUName for debugging
-            console.log('changeUName:', changeUName.changeUName);
+                // Assuming changeUName is asynchronous (returns a promise)
+                console.log(token);
+                console.log(name);
+                const success = await changeUName(name, token);
     
-            // Assuming changeUName is asynchronous (returns a promise)
-            const success = await changeUName(name, token);
-    
-            if (success) {
-                console.log('hi');
-                navigate('/dashboard');
-            } else {
-                setError('Login failed. Please check your credentials.');
-                console.log('wut');
+                if (success) {
+                    console.log('hi');
+                    navigate('/dashboard');
+                } else {
+                    setError('Login failed. Please check your credentials.');
+                    console.log('wut');
+                }
+            } catch (error) {
+                console.error('Error:', error);
+                setError('An error occurred. Please try again.');
             }
-        } catch (error) {
-            console.error('Error:', error);
-            setError('An error occurred. Please try again.');
         }
     };
+    
     
     return (
         <div className="profile-page">
